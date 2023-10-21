4e33b61f959a8c54cd996442637902875bec9b4c
Fernando Ramirez <gordsftrl@gmail.com>
2023-10-22 00:47:22 +0900

    added uv index to weather forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,14 +3,14 @@ const request = require('postman-request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=b09390fda62464a1b10fe88492cb2a77&query='+latitude+','+longitude
     
-    request({ url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, {body} = {}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees out.\nThe observation time is ' + body.current.observation_time + '.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees out.\nThe uv index is ' + body.current.uv_index + '.')
         }
     })
 }
